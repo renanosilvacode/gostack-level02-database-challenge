7f553acfc729b933d8146b2e6b059fdf24f79388
Renan Oliveira da Silva <renanosilva@gmail.com>
2021-01-19 14:50:26 -0300

    Delete transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -5,6 +5,7 @@ import TransactionsRepository from '../repositories/TransactionsRepository';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
@@ -36,7 +37,13 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+
+  const { id } = request.params;
+  const deleteTransactionService = new DeleteTransactionService();
+
+  await deleteTransactionService.execute(id);
+
+  return response.status(204).json();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+
+    const transactionRepository = getRepository(Transaction);
+
+    const transactionToRemove = await transactionRepository.findOne({ where: { id } });
+
+    if(!transactionToRemove){
+      throw new AppError('Transaction cannot be deleted', 400);
+    } else {
+
+      await transactionRepository.remove(transactionToRemove);
+    }
   }
 }
 
